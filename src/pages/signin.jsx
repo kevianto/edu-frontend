36635542c3c07c74952616c2885934df90c5fe3d
kevianto <kevinshahale746@gmail.com>
2025-03-23 14:16:48 +0300

    error correct

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -16,6 +16,7 @@ const SignIn = () => {
     try {
       const response = await fetch(`${API_URL}/auth/login`, {
         method: "POST",
+        
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
       });
